f1b25fc807ed513e86a53057edae5a4a2701bccc
curti <curti@DESKTOP-JDN2MDB>
2024-01-20 15:47:19 +0100

    asdas

--- Ubuntu.Server.API.Tests.DomainLayerTests/IonosServiceTest.cs
+++ Ubuntu.Server.API.Tests.DomainLayerTests/IonosServiceTest.cs
@@ -37,7 +37,7 @@ namespace Ubuntu.Server.API.Tests.DomainLayerTests
 
 			var result = ionosService.GetTheZoneID().Result;
 			 
-			Assert.IsNotNull(result);   
+			Assert.IsNotNull(result); // done
 			Assert.IsNotNull(expectedResult);
 			Assert.AreEqual(result, expectedResult);
 		}
